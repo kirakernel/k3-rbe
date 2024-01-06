e7708c8e4c95ad004f0ca4b28443fd8a179dc0e9
kirakernel <115137992+kirakernel@users.noreply.github.com>
2024-01-06 04:58:39 -0300

    Casting

---
+++ types/casting/src/main.rs
@@ -0,0 +1,58 @@
+#![allow(overflowing_literals)]
+
+fn main() {
+    let decimal = 65.4321_f32;
+
+    // Error! No implicit conversion
+    //let integer: u8 = decimal as u8;
+
+    //println!("{integer}");
+
+    // Explicit conversion
+    let integer = decimal as u8;
+    let character = integer as char;
+
+    //println!("{integer} and {character}");
+
+    // Error! There are limitations in conversion rules.
+    // A float cannot be directly converted to a char.
+    // let character = decimal as char;
+
+    println!("Casting: {} -> {} -> {}", decimal, integer, character);
+
+    // When casting any value to an unsigned type, T,
+    // T::MAX + 1 is added or subtracted until the value
+    // fits into the new type
+
+    // 1000 already fits in a u16
+    println!("1000 as a u16 is: {}", 1000 as u16);
+
+    // 1000 - 256 - 256 - 256 = 232
+    // Under the hood, the first 8 least significant bits (LSB) are kept,
+    // while the rest towards the most significant bit (MSB) get truncated.
+    println!("1000 as a u8 is : {}", 1000 as u8);
+    // -1 + 256 = 255
+    println!("  -1 as a u8 is : {}", (-1i8) as u8);
+
+    // For positive numbers, this is the same as modulus
+    println!("1000 mod 256 is : {}", 1000 % 256);
+
+    // Unless it already fits, of course.
+    println!(" 128 as a i16 is: {}", 128 as i16);
+
+    // In boundary case 128 value in 8-bit two's complement representation is -128
+    println!(" 128 as a i8 is : {}", 128 as i8);
+
+    // repeating the example above
+    // 1000 as u8 -> 238
+    println!("1000 as a u8 is : {}", 1000 as u8);
+    // and the value of 232 in 8-bit two's complement representation is -24
+    println!(" 232 as a i8 is : {}", 232 as i8);
+
+    // 300.0 as u8 is 255
+    println!(" 300.0 as u8 is : {}", 300.0_f32 as u8);
+    // -100.0 as u8 is 0
+    println!("-100.0 as u8 is : {}", -100.0_f32 as u8);
+    // nan as u8 is 0
+    println!("   nan as u8 is : {}", f32::NAN as u8);
+}
