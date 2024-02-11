f4c8c8f0f4c7abcf08d5b9df0600b5d2dd2385da
Taffarel Oliveira <taffareloliveira60@gmail.com>
2024-02-11 03:24:27 -0300

    Precision, localization and escaping

--- hello_world/std_fmt/src/main.rs
+++ hello_world/std_fmt/src/main.rs
@@ -154,6 +154,57 @@ fn main() {
     println!("{}", 0x00ff);
 
     // Precision
+    // For example, the following calls all print the same thing `Hello x is 0.01000:`
+    // Hello {arg 0 ("x")} is {arg 1 (0.01) with precision specified inline (5)}
+    println!("\n\nHello {0} is {1:.5}", "x", 0.01);
+
+    // Hello {arg 1 ("x")} is {arg 2 (0.01) with precision specified in arg 0 (5)}
+    println!("Hello {1} is {2:.0$}", 5, "x", 0.01);
+
+    // Hello {arg 0 ("x")} is {arg 2 (0.01) with precision specified in arg 1 (5)}
+    println!("Hello {0} is {2:.1$}", "x", 5, 0.01);
+
+    // Hello {next arg -> arg 0 ("x")} is {second of next two args -> arg 2 (0.01) with precision
+    //                          specified in first of next two args -> arg 1 (5)}
+    println!("Hello {} is {:.*}", "x", 5, 0.01);
+
+    println!(
+        "\n\n{}, `{name:.*}` has 3 fractional digits",
+        "Hello",
+        3,
+        name = 1234.56
+    );
+    println!(
+        "{}, `{name:.*}` has 3 characters",
+        "Hello",
+        3,
+        name = "1234.56"
+    );
+    println!(
+        "{}, `{name:>8.*}` has 3 right-aligned characters",
+        "Hello",
+        3,
+        name = "1234.56"
+    );
+
+    // Localization - https://doc.rust-lang.org/std/fmt/#localization
+    // Rust doesn't support different locales
+    println!("\n\nThe value is {} :(", 1.5);
+
+    // Escaping - https://doc.rust-lang.org/std/fmt/#escaping
+
+    let result = format!("\n\nHello {{}}"); // => "Hello {}"
+    println!("{}", result);
+
+    let result = format!("{{ Hello"); // =>  "{ Hello"
+    println!("{}", result);
+
+    let variable = "hehehe!!";
+    println!("{{variable}}");
+    println!("{variable}");
+    // println!("{{}}", variable);
+
+    // Syntax - https://doc.rust-lang.org/std/fmt/#syntax
 }
 
 fn make_string(a: u32, b: &str) -> String {
